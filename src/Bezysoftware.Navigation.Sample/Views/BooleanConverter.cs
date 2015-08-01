07569729a4efe0fd548c4689239229c3d19da060
Tomas Bezouska <bezysoftware@hotmail.cz>
2015-08-01 13:51:21 +0200

    Added missing files

---
+++ src/Bezysoftware.Navigation.Sample/Views/BooleanConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Bezysoftware.Navigation.Sample.Views
+{
+    using System;
+    using Windows.UI.Xaml.Data;
+
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+    }
+}
