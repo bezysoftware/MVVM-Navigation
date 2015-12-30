9c521760d7989137d5210228e80561dbc5e40c5f
bezysoftware <bezysoftware@outlook.com>
2015-12-30 18:59:47 +0100

    Navigation Commands implementing ICommand

---
+++ src/Bezysoftware.Navigation/UI/GoBackCommand.cs
@@ -0,0 +1,58 @@
+﻿namespace Bezysoftware.Navigation.UI
+{
+    using Lookup;
+    using Microsoft.Practices.ServiceLocation;
+    using System;
+    using System.Windows.Input;
+    using Windows.UI.Xaml;
+
+    public class GoBackCommand : DependencyObject, ICommand
+    {
+        public static readonly DependencyProperty ActivationDataProperty = DependencyProperty.Register("ActivationData", typeof(object), typeof(NavigateToViewAction), new PropertyMetadata(null));
+        public static readonly DependencyProperty NavigationServiceProperty = DependencyProperty.Register("NavigationService", typeof(INavigationService), typeof(NavigateToViewAction), new PropertyMetadata(null));
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// The navigation service. If not provided, the instance is resolved using <see cref="ServiceLocator"/>.
+        /// </summary>
+        public INavigationService NavigationService
+        {
+            get { return (INavigationService)GetValue(NavigationServiceProperty); }
+            set { SetValue(NavigationServiceProperty, value); }
+        }
+
+        /// <summary>
+        /// Data to pass to target ViewModel.
+        /// </summary>
+        public object ActivationData
+        {
+            get { return (object)GetValue(ActivationDataProperty); }
+            set { SetValue(ActivationDataProperty, value); }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var service = this.GetNavigationService();
+        
+            if (this.ActivationData == null)
+            {
+                service.GoBack();
+            }
+            else
+            {
+                service.GoBack(this.ActivationData);
+            }
+        }
+
+        private INavigationService GetNavigationService()
+        {
+            return this.NavigationService ?? ServiceLocator.Current.GetInstance<INavigationService>();
+        }
+    }
+}
---
+++ src/Bezysoftware.Navigation/UI/NavigateCommand.cs
@@ -0,0 +1,74 @@
+﻿namespace Bezysoftware.Navigation.UI
+{
+    using Lookup;
+    using Microsoft.Practices.ServiceLocation;
+    using System;
+    using System.Windows.Input;
+    using Windows.UI.Xaml;
+
+    public class NavigateCommand : DependencyObject, ICommand
+    {
+        public static readonly DependencyProperty TargetViewTypeProperty = DependencyProperty.Register("TargetViewType", typeof(Type), typeof(NavigateToViewAction), new PropertyMetadata(null));
+        public static readonly DependencyProperty ActivationDataProperty = DependencyProperty.Register("ActivationData", typeof(object), typeof(NavigateToViewAction), new PropertyMetadata(null));
+        public static readonly DependencyProperty NavigationServiceProperty = DependencyProperty.Register("NavigationService", typeof(INavigationService), typeof(NavigateToViewAction), new PropertyMetadata(null));
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// The navigation service. If not provided, the instance is resolved using <see cref="ServiceLocator"/>.
+        /// </summary>
+        public INavigationService NavigationService
+        {
+            get { return (INavigationService)GetValue(NavigationServiceProperty); }
+            set { SetValue(NavigationServiceProperty, value); }
+        }
+
+        /// <summary>
+        /// Data to pass to target ViewModel.
+        /// </summary>
+        public object ActivationData
+        {
+            get { return (object)GetValue(ActivationDataProperty); }
+            set { SetValue(ActivationDataProperty, value); }
+        }
+
+        /// <summary>
+        /// Type of View to navigate to.
+        /// </summary>
+        public Type TargetViewType
+        {
+            get { return (Type)GetValue(TargetViewTypeProperty); }
+            set { SetValue(TargetViewTypeProperty, value); }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var service = this.GetNavigationService();
+            var viewModelType = this.GetViewModelType();
+
+            if (this.ActivationData == null)
+            {
+                service.Navigate(viewModelType);
+            }
+            else
+            {
+                service.Navigate(viewModelType, this.ActivationData);
+            }
+        }
+
+        private Type GetViewModelType()
+        {
+            return ServiceLocator.Current.GetInstance<IViewModelLocator>().GetAssociatedViewModelType(this.TargetViewType);
+        }
+
+        private INavigationService GetNavigationService()
+        {
+            return this.NavigationService ?? ServiceLocator.Current.GetInstance<INavigationService>();
+        }
+    }
+}
